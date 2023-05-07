315d766be094f097a400e5b92f11c9e3bcbd3035
Nadzieja Hamerlak <nadia.hamerlak@gmail.com>
2023-05-07 02:19:30 +0200

    Weight and cmax that works

--- NEH/NEH.cpp
+++ NEH/NEH.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 
 using namespace std;
-int* weight(int* Table, int* Order, int n, int m) {
+int* weight(int* Table, int n, int m) {
     int* Weights = new int[n];
     for (int i = 0; i < n; i++) {
         Weights[i] = 0;
@@ -12,33 +12,36 @@ int* weight(int* Table, int* Order, int n, int m) {
     }
     return Weights;
 }
-void sort(int* Order, int* Weights, int n, int m) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            if (Weights[j] < Weights[j + 1]){
-                swap(Weights[j], Weights[j + 1]);
-                swap(Order[j], Order[j + 1]);
-            }
-        }
-    }
-}
+
 int cmax(int* Table, int* Order, int n, int m) {
-    int* C = new int[m];
-    for (int i = 0; i <= m; i++)
-    {
+    int* C = new int[m*n];
+    for (int i = 0; i < m*n; i++) {
         C[i] = 0;
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            C[i] = max(C[j], C[j - 1]) + Table[(j - 1) + Order[i] * m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+        {      
+            if (i == 0 and j == 0) {
+                C[i * m + j] = Table[(Order[i] * m + j)];
+            }
+            else if (i == 0) {
+                C[i * m + j] = C[(i * m + j)-1] + Table[(Order[i] * m + j)];
+            }
+            else if (j == 0) {
+                C[i * m + j] = C[((i-1) * m + j)] + Table[(Order[i] * m + j)];
+            }else{
+                C[i * m + j] = max(C[((i - 1) * m + j)], C[(i * m + j) - 1]) + Table[(Order[i] * m + j)];
+            }
+            
+            //cout << i * m + j << "    " << C[i * m + j] << endl;
         }
+
     }
-    return C[m];
+    return C[(m*n)-1];
 }
 int algorithm(int* Table, int* Order, int n, int m) {
-    return 1;
+    int* Weigths = weight(Table, n, m);
+
 }
 int main()
 {
@@ -51,7 +54,7 @@ int main()
             if (s.find("data") != string::npos) {
                 cout << s << endl;
             }
-         
+
         }
         f >> n;
         f >> m;
@@ -66,13 +69,18 @@ int main()
         }
         for (int j = 0;j < n;j++) {
             for (int k = 0;k < m;k++) {
-                cout << Table[j * m + k] << " ";
+                //cout << Table[j * m + k] << " ";
             }
-            cout << endl;
-            
+            //cout << endl;
+
         }
-        cout << algorithm(Table, Order, n, m) << endl;
-        weight(Table, Order, n, m);
+        cout << cmax(Table, Order, n, m) << endl;
+        //int* weights = weight(Table, Order, n, m);
+        //cout << algorithm(Table, Order, n, m) << endl;
+        /*for (int j = 0;j < n;j++) {
+            cout << weights[j] << endl;
+
+        }*/
         delete[] Table;
 
     }
